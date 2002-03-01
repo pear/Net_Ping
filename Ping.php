d6351442ed2bcb0925f7c48fbc77c1bd63d9ac71
Tomas V.V.Cox <cox@php.net>
2002-03-01 04:10:44 +0000

    - Added the checkHost() method - Some pearifing work - Go with 0.2 release # Martin not problem with the stuff I've introduced no? :-)
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_Ping/trunk@71463 c90b9560-bf6c-de11-be94-00142212c4b1

--- Ping.php
+++ Ping.php
@@ -14,12 +14,14 @@
 // | license@php.net so we can mail you a copy immediately.               |
 // +----------------------------------------------------------------------+
 // | Authors: Martin Jansen <mj@php.net>                                  |
+// |          Tomas V.V.Cox <cox@idecnet.com>                             |
+// |                                                                      |
 // +----------------------------------------------------------------------+
 //
 // $Id$
 
-define(PING_FAILED,"execution of ping failed");
-define(HOST_NOT_FOUND,"unknown host");
+define('PING_FAILED',    'execution of ping failed');
+define('HOST_NOT_FOUND', 'unknown host');
 
 /**
 * Wrapper class for ping calls
@@ -41,46 +43,85 @@ define(HOST_NOT_FOUND,"unknown host");
 * @package  Net
 * @access   public
 */
-class Net_Ping {
+class Net_Ping
+{
 
     /**
     * Location where the ping program is stored
     * @var string
     */
     var $ping_path = "ping";
-    
+
     /**
     * Array with the result from the ping execution
     * @var array
     */
     var $result = array();
-        
+
     /**
     * Execute ping
     *
     * @param  string    $host           hostname
     * @param  int       $count          count of the pings
     * @param  int       $packet_size    ping packet size
+    * @param  bool      $quiet          to set or not the "-q" ping param
     * @return mixed  String on error or array with the result
     * @access public
     */
-    function ping($host,$counts = 5,$packet_size = 32) {
-        
-        $param = $this->ping_path." -c ".$counts." -s ".$packet_size." ".$host;
-                               
-        exec($param, $result);
-        
+    function ping($host, $counts = 5, $packet_size = 32, $quiet = false)
+    {
+        $q = $quiet ? '-q' : '';
+        $cmd = $this->ping_path." $q -c $counts -s $packet_size $host";
+
+        exec($cmd, $result);
+
         if (!is_array($result)) {
             return PING_FAILED;
         }
-        
+
         if (count($result) == 0) {
             return HOST_NOT_FOUND;
         } else {
             $this->result = $result;
             return $result;
         }
-    }   
-    
+    }
+
+    /**
+    * Check if a host is up by pinging it
+    *
+    * @param string $host   The host to test
+    * @param bool $severely If some of the packages did reach the host
+    *                       and severely is false the function will return true
+    * @return bool True on success or false otherwise
+    *
+    * @notes Output taken from ping of netkit-base-0.10-37
+    */
+    function checkHost($host, $severely = true)
+    {
+        $res = $this->ping($host, 10, 32, true);
+        if ($res == HOST_NOT_FOUND) {
+            return false;
+        }
+        $line = preg_split('|\s+|', $res[3]);
+        if (!is_numeric($line[0]) || !is_numeric($line[3]) ||
+            $line[2] != 'transmitted,' || $line[5] != 'received,') {
+            ob_start();
+            var_dump($line);
+            $rep = ob_get_contents();
+            ob_end_clean();
+            trigger_error("Output format seems not to be supported, please report ".
+                          "the following to pear-dev@lists.php.net, including your ".
+                          "version of ping or netkit-base:\n $rep");
+            return false;
+        }
+        // [0] => transmitted, [3] => received
+        if ($line[0] != $line[3]) {
+            if ($line[3] == 0 || $severely) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 ?>
