7bef588d7d12f52b20949c19b8dd3212a70458b6
Christian Weiske <cweiske@cweiske.de>
2019-08-25 18:19:18 +0200

    Fix file docblock in Net/Ping.php; align license with composer.json

--- Net/Ping.php
+++ Net/Ping.php
@@ -1,26 +1,19 @@
 <?php
-//
-// +----------------------------------------------------------------------+
-// | PHP Version 4                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2004 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the PHP license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: Martin Jansen <mj@php.net>                                  |
-// |          Tomas V.V.Cox <cox@idecnet.com>                             |
-// |          Jan Lehnardt  <jan@php.net>                                 |
-// |          Kai Schrder <k.schroeder@php.net>                          |
-// |          Craig Constantine <cconstantine@php.net>                    |
-// +----------------------------------------------------------------------+
-//
-// $Id$
+/**
+ * Part of the Net_Ping package
+ *
+ * PHP version 4, 5 and 7
+ *
+ * @category Networking
+ * @package  Net_Ping
+ * @author   Martin Jansen <mj@php.net>
+ * @author   Tomas V.V.Cox <cox@idecnet.com>
+ * @author   Jan Lehnardt <jan@php.net>
+ * @author   Kai Schröder <k.schroeder@php.net>
+ * @author   Craig Constantine <cconstantine@php.net>
+ * @license  http://www.php.net/license/3_01.txt PHP-3.01
+ * @link     https://pear.php.net/package/Net_Ping
+ */
 
 require_once "PEAR.php";
 require_once "OS/Guess.php";
@@ -53,11 +46,6 @@ define('NET_PING_RESULT_UNSUPPORTED_BACKEND', 4);
 *     var_dump($ping->ping('example.com'));
 *   }
 * ?>
-*
-* @author   Jan Lehnardt <jan@php.net>
-* @version  $Revision$
-* @package  Net
-* @access   public
 */
 class Net_Ping
 {
